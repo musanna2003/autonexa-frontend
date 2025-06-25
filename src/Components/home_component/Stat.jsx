fb33dc13b787ede8c77221a3d8d6bf619fb4b375
musanna2003 <abumusanna2003@gmail.com>
2025-06-25 08:36:42 +0600

    add stat section

--- src/Components/home_component/Stat.jsx
+++ src/Components/home_component/Stat.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import CountUp from 'react-countup';
 
 const Stat = () => {
     return (
-        <div>
-            
+        <div className='w-[90vw] md:w-[90vw] mx-auto space-y-10'>
+            <div className="stats stats-vertical w-full md:stats-horizontal shadow">
+
+                <div className="stat">
+                    <div className="w-full flex flex-col justify-center items-center">
+                        <div className="stat-title">Total Cars</div>
+                        <div className="stat-value">
+                            <CountUp start={0} end={199} duration={2} enableScrollSpy scrollSpyDelay={100} />+
+                        </div>
+                        <div className="stat-desc">Listed for rent</div>
+                    </div>
+                </div>
+
+                <div className="stat">
+                    <div className="w-full flex flex-col justify-center items-center">
+                        <div className="stat-title">Successful Bookings</div>
+                        <div className="stat-value">
+                            <CountUp start={0} end={999} duration={2} enableScrollSpy scrollSpyDelay={100} />
+                        </div>
+                        <div className="stat-desc">Made by customers</div>
+                    </div>
+                </div>
+
+                <div className="stat">
+                    <div className="w-full flex flex-col justify-center items-center">
+                        <div className="stat-title">Customer Satisfaction Rate</div>
+                        <div className="stat-value">
+                            <CountUp start={0} end={90} duration={2} enableScrollSpy scrollSpyDelay={100} />%
+                        </div>
+                        <div className="stat-desc">Positive feedback</div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 };
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,6 +4,7 @@ import RecentCar from '../Components/home_component/RecentCar';
 import Why_Choose_Us from '../Components/home_component/Why_Choose_Us';
 import SpecialOffers from '../Components/home_component/SpecialOffers';
 import { useLoaderData } from 'react-router';
+import Stat from '../Components/home_component/Stat';
 
 const Home = () => {
 
@@ -13,6 +14,7 @@ const Home = () => {
         <div className='space-y-16'>
             <Hero></Hero>
             <Why_Choose_Us></Why_Choose_Us>
+            <Stat></Stat>
             <RecentCar cars={cars}></RecentCar>
             <SpecialOffers></SpecialOffers>
         </div>
